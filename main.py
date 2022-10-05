83868e41b5af87222a295263e7c0a812a6b4eba4
Admin <shura.x@mail.ru>
2022-10-05 21:42:50 +0300

    Продолжил изучать openCV

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import cv2
 import numpy
 
-img=cv2.imread('Face.jpg')
-img = cv2.resize(img, (img.shape[1] // 2, img.shape[0] // 2))
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-img = cv2.Canny(img, 250, 250)
-kernel = numpy.ones((5, 5), numpy.uint8)
-img = cv2.dilate(img, kernel, iterations=1)
-img = cv2.erode(img, kernel, iterations=1)
+img=cv2.imread('Face.jpg', cv2.IMREAD_GRAYSCALE) # Загрузить изображение в черно-белом цвете
+# img = cv2.resize(img, (img.shape[1] // 2, img.shape[0] // 2)) # Увеличить или уменьшить изображение
+# img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # преобразование изображения в черно-белый цвет
+img = cv2.GaussianBlur(img, (5,5), 0) # Размытие изображения
+img = cv2.Canny(img, 100, 150) # Оставляем только черный и белый цвет, без оттенков
+# kernel = numpy.ones((5, 5), numpy.uint8)
+# img = cv2.dilate(img, kernel, iterations=1)
+# img = cv2.erode(img, kernel, iterations=1)
 
-cv2.rectangle(img, (50,50),(300,300), (255,100,80),thickness=3)
+# print(cv2.findContours(img, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)[0][0])
 
 cv2.imshow('Face',img)
-cv2.waitKey(0)
+if cv2.waitKey(0) == ord('q'):
+    cv2.destroyAllWindows()
 """video = cv2.VideoCapture(0)
 video.set(3, 500)
 video.set(4, 300)
