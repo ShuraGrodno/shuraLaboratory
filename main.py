91939c0f909acc1c191225ededec0714066adc42
Admin <shura.x@mail.ru>
2022-11-01 22:20:42 +0300

    Проведена проверка определение лица на видео потоке переписан код под видео обработку лица

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy
 
-img=cv2.imread('Face.jpg', cv2.IMREAD_GRAYSCALE) # Загрузить изображение в черно-белом цвете
+# img=cv2.imread('Face.jpg', cv2.IMREAD_GRAYSCALE) # Загрузить изображение в черно-белом цвете
 # img = cv2.resize(img, (img.shape[1] // 2, img.shape[0] // 2)) # Увеличить или уменьшить изображение
 # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # преобразование изображения в черно-белый цвет
 # img = cv2.GaussianBlur(img, (5, 5), 0) # Размытие изображения
@@ -12,20 +12,27 @@ img=cv2.imread('Face.jpg', cv2.IMREAD_GRAYSCALE) # Загрузить изобр
 
 # print(cv2.findContours(img, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)[0][0])
 
-faces=cv2.CascadeClassifier('faces_detect.xml')
-rezult=faces.detectMultiScale(img, scaleFactor=1.2, minNeighbors=3)
-
-for (x,y,w,h) in rezult:
-    cv2.rectangle(img, (x,y),(x+w,y+h),(0,0,255),thickness=3)
-
-cv2.imshow('Face',img)
-
-if cv2.waitKey(0) == ord('q'):
-    cv2.destroyAllWindows()
-"""video = cv2.VideoCapture(0)
+# faces=cv2.CascadeClassifier('faces_detect.xml')
+# rezult=faces.detectMultiScale(img, scaleFactor=1.2, minNeighbors=3)
+#
+# for (x,y,w,h) in rezult:
+#     cv2.rectangle(img, (x,y),(x+w,y+h),(0,0,255),thickness=2)
+#
+# cv2.imshow('Face',img)
+#
+# if cv2.waitKey(0) == ord('q'):
+#     cv2.destroyAllWindows()
+video = cv2.VideoCapture(0)
 video.set(3, 500)
 video.set(4, 300)
-while True:
+faces=cv2.CascadeClassifier('faces_detect.xml')
+cikle = True
+while cikle:
     _, img = video.read()
+    rezult=faces.detectMultiScale(img, scaleFactor=1.2, minNeighbors=1)
+    for (x, y, w, h) in rezult:
+        cv2.rectangle(img, (x,y),(x+w,y+h),(0,0,255),thickness=2)
     cv2.imshow('Video', img)
-    cv2.waitKey(1)"""
+    if cv2.waitKey(1) == ord('q'):
+        cikle = False
+        cv2.destroyAllWindows()
